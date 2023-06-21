a362515ad8f74a461faa639ba58ca9592d6cf7c9
abi <abi.rahmawan01@students.uns.ac.id>
2023-06-21 23:49:18 +0700

    up catalogue

--- client/src/components/catalogue/CatalogueList.js
+++ client/src/components/catalogue/CatalogueList.js
@@ -1,40 +1,53 @@
-import { Container, Row, Col } from "react-bootstrap";
 import "./Catalogue.css";
 
+import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { getProduct } from "./allProductData";
-
-import foto from "../teh.jpg";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 const CatalogueList = () => {
-  let product = getProduct();
+
+  const [bevList, setBevList] = useState([]);
+  
+  useEffect(() => {
+    Axios.get("http://localhost:5000/bevs").then((response) => {
+      //console.log(response.data);
+      setBevList(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      console.log(error.response);
+      console.log(error.message);
+    });
+  }, []);
+
   let currentType = "";
 
   return (
     <>
     <h2 className="header container-fluid mt-2 fw-bold">Our Catalogue</h2>
-      {product.map((product, index) => {
-        if (product.type !== currentType) {
-          currentType = product.type;
+      {bevList.map((val, index) => {
+        if (val.type !== currentType) {
+          currentType = val.type;
           return (
             <Container fluid className="menu__box-green" key={currentType}>
-              <h3 className="type-menu-green">{product.type}</h3>
+              <h3 className="type-menu-green">{val.type}</h3>
               <h4 className="dash-green"></h4>
-              <Link to={`/product-details-catalogue/${product.number}`} key={product.number}>
+              <Link to={`/product-details-catalogue/${val.id}`} key={val.id}>
                 <Row className="list-menu-green">
                   <Col md={10} xs={8}>
-                    <h5 className="code-name-green">{product.name}</h5>
+                    <h5 className="code-name-green">{val.name}</h5>
                     <small className="status">
                       <i className="bi bi-hand-thumbs-up me-2"></i>
-                      {product.highlight}
+                      {val.highlight}
                     </small>
-                    <p>{product.ings}</p>
-                    <p className="fw-bold">Rp28.000</p>
+                    <p>{val.ings}</p>
+                    <p className="fw-bold">Rp{val.price}</p>
                   </Col>
 
                   <Col md={2} xs={4} className="mt-3">
                     <div className="position-relative" style={{ height: "88px" }}>
-                      <img src={foto} width={"88px"} height={"88px"} className="float-end" style={{ borderRadius: "20px" }}></img>
+                      <img alt={val.name} src={`/bev-img/${val.img1}`} width={"88px"} height={"88px"} className="float-end" style={{ borderRadius: "20px" }}></img>
                     </div>
                   </Col>
                 </Row>
@@ -45,21 +58,21 @@ const CatalogueList = () => {
           return (
             <Container fluid className="menu__box-green" style={{ boxShadow: "0 !important", marginTop: "-11px", zIndex: "10" }}>
               {index !== 0 && <h4 className="dash-green"></h4>}
-              <Link to={`/product-details-catalogue/${product.number}`} key={product.number}>
-                <Row className="list-menu-green" key={product.number}>
+              <Link to={`/product-details-catalogue/${val.id}`} key={val.id}>
+                <Row className="list-menu-green" key={val.id}>
                   <Col md={10} xs={8}>
-                    <h5 className="code-name-green">{product.name}</h5>
+                    <h5 className="code-name-green">{val.name}</h5>
                     <small className="status">
                       <i className="bi bi-hand-thumbs-up me-2"></i>
-                      {product.highlight}
+                      {val.highlight}
                     </small>
-                    <p>{product.ings}</p>
-                    <p className="fw-bold">Rp28.000</p>
+                    <p>{val.ings}</p>
+                    <p className="fw-bold">Rp{val.price}</p>
                   </Col>
 
                   <Col md={2} xs={4} className="mt-3">
                     <div className="position-relative" style={{ height: "88px" }}>
-                      <img src={foto} width={"88px"} height={"88px"} className="float-end" style={{ borderRadius: "20px" }}></img>
+                      <img alt={val.name} src={`/bev-img/${val.img1}`} width={"88px"} height={"88px"} className="float-end" style={{ borderRadius: "20px" }}></img>
                     </div>
                   </Col>
                 </Row>
--- client/src/components/catalogue/DescryptionCatalogue.js
+++ client/src/components/catalogue/DescryptionCatalogue.js
@@ -1,62 +1,73 @@
-import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import './Catalogue.css';
 
 import { useParams } from "react-router-dom";
-import { getProducts } from "./allProductData";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import Card from "react-bootstrap/Card";
+import Badge from "react-bootstrap/Badge";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 function BodyOnlyExample() {
-  let params = useParams();
-  let products = getProducts(parseInt(params.allId, 10));
+
+  const { id } = useParams();
+  const [val, setVal] = useState({id});
+  console.log(id);
+  
+  useEffect(() => {
+    Axios.get(`http://localhost:5000/bevs/${val.id}`).then((response) => {
+      //console.log(response.data);
+      setVal(response.data);
+    });
+  }, [id]);
+
   return (
     <div className='desc-box'>
     <Card className='shadow-none desc-body'>
       <Card.Body>
         <div className='mx-1'>
           <Badge className='badge-custom-green fw-normal ms-2'>
-            {products.type}
+            {val.type}
           </Badge>
           <Badge className='badge-custom-green fw-normal ms-2'>
-            {products.ings[0]}
+            {val.highlight}
           </Badge>
           <Badge className='badge-custom-green fw-normal ms-2'>
             Angry
           </Badge>
         </div>
 
-        <h2 className='ms-2 mt-2 fw-bold'>{products.name}</h2>
+        <h2 className='ms-2 mt-2 fw-bold'>{val.name}</h2>
         
         <div className="ms-2">
-          <span className="type">{products.highlight}</span>
+          <span className="type">{val.highlight}</span>
         </div>
 
         <Container fluid>
         <Row className='ings mt-3'>
           <ul className=' d-flex justify-content-between'>
-            <li>
-              <i className="bi bi-database me-2"></i>
-              {products.brew[0]}
-            </li>
-            <li>
-              <i className="bi bi-cup-straw me-2"></i>
-              {products.brew[1]}
-            </li>
-            <li>
-              <i className="bi bi-thermometer-half me-2"></i>
-              {products.brew[2]}
-            </li>
-            <li>
-              <i className="bi bi-hourglass-bottom me-2"></i>
-              {products.brew[3]}
-            </li>
+                <li>
+                  <i className="bi bi-database me-2"></i>
+                  {val.tsp} Teaspoon ({val.tspg}g)
+                </li>
+                <li>
+                  <i className="bi bi-cup-straw me-2"></i>
+                  {val.water} Ml
+                </li>
+                <li>
+                  <i className="bi bi-thermometer-half me-2"></i>
+                  {val.temp} °C
+                </li>
+                <li>
+                  <i className="bi bi-hourglass-bottom me-2"></i>
+                  {val.time} Mins
+                </li>
           </ul>
         </Row>
         </Container>
         <hr></hr>
-        <p className='ms-2'>{products.highlight}</p>
-        <p className='ms-2'>{products.desc}</p>
+        <p className='ms-2'>{val.highlight}</p>
+        <p className='ms-2'>{val.desc}</p>
       </Card.Body>
     </Card>
     <div className="half-circle"></div>
